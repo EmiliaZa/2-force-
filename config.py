dc7ba7b1697943943e7900ca4580990c9790c5de
EmiliaZa <159568139+EmiliaZa@users.noreply.github.com>
2024-02-23 14:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Emilia")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001886813820"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001870999497"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001717121807"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
